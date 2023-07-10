50bd076fcade0c1cf253474c9d8bcd37d0e691b4
NamLh <110580153+LeHaNam1142003@users.noreply.github.com>
2023-07-10 11:00:00 +0700

    Update Rope.cs

--- Assets/_Project/Scripts/_GamePlay/Rope.cs
+++ Assets/_Project/Scripts/_GamePlay/Rope.cs
@@ -1,13 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Obi;
 using Pancake;
 using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
     [ReadOnly] public bool iscollide;
+    [SerializeField] private bool isShowLength;
+    private float _maxLength;
+    [ReadOnly] public bool isCheckLength;
     [ReadOnly] public bool isDone;
+    [SerializeField] private Material normal;
+    [SerializeField] private Material maxLength;
+    [SerializeField] private MeshRenderer ropeMesh;
+    private ObiRope _rope;
+    private void Start()
+    {
+        _rope = gameObject.GetComponent<ObiRope>();
+        _maxLength = Mathf.CeilToInt(_rope.CalculateLength()) + 0.8f;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(Constant.Rope))
@@ -18,6 +31,28 @@ public class Rope : MonoBehaviour
         if (other.gameObject.CompareTag(Constant.Point))
         {
             Observer.RopeCheck?.Invoke(this);
+        }
+    }
+    private void Update()
+    {
+        var a = gameObject.GetComponent<ObiRope>();
+        if (isShowLength)
+        {
+            Debug.Log(a.CalculateLength());
+            Debug.Log(_maxLength);
+        }
+        if (_rope.CalculateLength() > _maxLength)
+        {
+            ropeMesh.material = maxLength;
+            if (isCheckLength)
+            {
+                Observer.MaxLength?.Invoke();
+            }
+
+        }
+        else
+        {
+            ropeMesh.material = normal;
         }
     }
 }
