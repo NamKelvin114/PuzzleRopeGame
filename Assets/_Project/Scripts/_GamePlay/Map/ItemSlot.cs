0123818d0ac2fdc686701bf2e90e57c9af9dc5fc
NamLh <namkelvin114@gmail.com>
2023-07-12 16:52:45 +0700

    Update ItemSlot.cs

--- Assets/_Project/Scripts/_GamePlay/Map/ItemSlot.cs
+++ Assets/_Project/Scripts/_GamePlay/Map/ItemSlot.cs
@@ -1,9 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemSlot : MonoBehaviour
 {
+    [SerializeField] private Color colorSelected;
+    [SerializeField] private Color normalColor;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        SetSelected(false);
+    }
+    public void SetSelected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            spriteRenderer.color = colorSelected;
+        }
+        else
+        {
+            spriteRenderer.color = normalColor;
+        }
+    }
     public bool isCollide;
     public int row;
     public int column;
